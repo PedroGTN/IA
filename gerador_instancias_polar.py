b3253f57809c2cac5fe2ceb8e8d5ca1e11a63718
PedroGTN <pedrogtneves@gmail.com>
2025-01-18 10:21:18 -0300

    Coordenadas polares adicionadas

--- gerador_instancias_polar.py
+++ gerador_instancias_polar.py
@@ -10,6 +10,7 @@ import os
 import sys
 import random as rnd
 import time
+import math
 
 #trocando folder
 inst_folder = sys.argv[3]
@@ -32,26 +33,16 @@ for inst in range(inst_num):
     #abrindo arquivo de instancias
     with open("inst"+str(inst)+".in", 'w') as arq:
 
-        #criando variaveis que seram usadas para calcular as coordenadas
-        loc_x = 0
-        loc_y = 0
-
         #colocando o tamanho da instancia na primeira linha
         arq.write(str(inst_size) + '\n')
 
         for i in range(inst_size):
                 
-                #numero aleatorio de -1 a 1
-                loc_x = rnd.uniform(-1, 1)
-
-                #calculando coordenada correspondente
-                loc_y = ((1 - loc_x**2)**0.5) * (1 if rnd.randint(0, 1) else -1)
-
-                #processamento para variedade e tirar negativos para x e y
-                loc_x*= desloc
-                loc_x += desloc + desloc_div3 + rnd.uniform(-desloc_div3, desloc_div3)
-                loc_y *= desloc
-                loc_y += desloc + desloc_div3 + rnd.uniform(-desloc_div3, desloc_div3)
+                angle = rnd.uniform(0, 360)
+                radius = desloc + desloc_div3 + rnd.uniform(-desloc_div3, desloc_div3)
+                
+                loc_x = math.cos(angle) * radius
+                loc_y = math.sin(angle) * radius
 
                 #escrevendo resultado no arquivo
                 arq.write(str(loc_x) + ' ' + str(loc_y) + '\n')
