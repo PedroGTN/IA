4a7e918ca612731b147d5a73c42af822b697ddb3
PedroGTN <pedrogtneves@gmail.com>
2025-01-18 10:23:00 -0300

    Comentários nas coordenadas polares

--- T1/gerador_instancias_polar.py
+++ T1/gerador_instancias_polar.py
@@ -38,9 +38,11 @@ for inst in range(inst_num):
 
         for i in range(inst_size):
                 
+                #gerando um angulo e um raio para definir a posição do nó
                 angle = rnd.uniform(0, 360)
                 radius = desloc + desloc_div3 + rnd.uniform(-desloc_div3, desloc_div3)
                 
+                #usando seno e cosseno para definir as coordenadas x e y do nó
                 loc_x = math.cos(angle) * radius
                 loc_y = math.sin(angle) * radius
 
